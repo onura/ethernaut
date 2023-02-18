cea1e92bfde22b2a1e4e53a34dbf3138a6870a02
ospwner <{ID}+{username}@users.noreply.github.com>
2023-02-18 22:50:48 +0300

    Add DoubleEntryPoint solution

--- script/DoubleEntryPoint.s.sol
+++ script/DoubleEntryPoint.s.sol
@@ -2,36 +2,72 @@
 pragma solidity ^0.8.0;
 
 import "forge-std/Script.sol";
+import "forge-std/console.sol";
 import "../src/DoubleEntryPoint.sol";
-import "../src/DoubleEntryPointSolution.sol";
 
 // anvil -f $GOERLI_RPC_URL --fork-block-number 8512137
 // forge script script/DoubleEntryPoint.s.sol:DoubleEntryPointSolution --fork-url http://127.0.0.1:8545 --broadcast -vvv
 
 contract DoubleEntryPointSolution is Script {
-    address internal constant LEGACY_ADDR = 0x0B504017e7950ce5C002cF3983eDa10Ac0D823C5;
-    address internal constant DOUBLE_ENT_ADDR = 0xDb9c42DD6bFf1af89fcD9A02A2D9145e8F69804A;
+    address internal constant LEGACY_ADDR = 0xB508405BE25B95ba3e02dC7bc0F0B316cce8c838;
+    address internal constant DOUBLE_ENT_ADDR = 0x5C5BB6030B6579701CD38b4CDD84C2c0cd57a57e;
 
     DoubleEntryPoint public dep;
     CryptoVault public vault;
     Forta public forta;
-    DoubleEntryPointSolution public solution;
+    DetectionBot public bot;
 
-    function setup() {
+    function setUp() public {
         dep = DoubleEntryPoint(DOUBLE_ENT_ADDR);
         vault = CryptoVault(dep.cryptoVault());
         forta = Forta(dep.forta());
-        solution = new DoubleEntryPointSolution(address(forta));
     }
     
-    function run() {
-        uint256 attackerPK = vm.envUint("ANVIL_PRIVATE_KEY");
-        //uint256 attackerPK = vm.envUint("PRIVATE_KEY");
+    /*
+     * Cryptovault's sweepToken function tries to prevent transferring underlying
+     * token by a require check. However, it accepts and ERC20 token and calls its' transfer function.
+     * LegacyToken's transfer function calls the DoubleEntryPoint which is the underlying token's 
+     * delegateTransfer function. If an attacker calls the sweepToken with LegacyToken argument argument,
+     * Cyrptovault gracefully transfers the residual underlying tokens to the sweptTokenRecipient.
+     */
+    function run() public {
+        //uint256 attackerPK = vm.envUint("ANVIL_PRIVATE_KEY");
+        uint256 attackerPK = vm.envUint("PRIVATE_KEY");
 
-        vm.broadcast(attackerPK);
-        forta.setDetectionBot(address(solution));
-        vault.sweepToken(LEGACY_ADDR);
+        vm.startBroadcast(attackerPK);
+
+        // Deploy the detection bot
+        bot = new DetectionBot(address(forta), vault.sweptTokensRecipient());
+        
+        // Register the detection bot
+        forta.setDetectionBot(address(bot));
+        
+        // Trigger sweepToken vulnerability to test the bot
+        //vault.sweepToken(IERC20(LEGACY_ADDR));
 
         vm.stopBroadcast();
+    }
+}
+
+contract DetectionBot is IDetectionBot {
+
+    IForta private forta;
+    address sweepReceiver;
+
+    constructor (address fortaAddr, address _sweepReceiver) {
+        forta = IForta(fortaAddr);
+        sweepReceiver = _sweepReceiver;
+    }
+
+    function handleTransaction(address user, bytes calldata msgData) override external {
+        address to;
+        uint256 value;
+        address origSender;
+        (to, value, origSender) = abi.decode(msgData[4:],(address,uint256,address));
+
+        // Underlying token should not be transferred to sweepTokenRecipient 
+        if (to == sweepReceiver) {
+            forta.raiseAlert(user);
+        }
     }
 }
